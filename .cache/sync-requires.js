2a0c67a18edf5cba6c34e0205fc3e05f2f80e6eb
Luca Kiebel <lucakiebel@xenoncloud.net>
2021-10-28 22:28:46 +0200

    build?

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/luca/Desktop/work/nibyou/code/k-b.dev/.cache/dev-404-page.js")),
   "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("/Users/luca/Desktop/work/nibyou/code/k-b.dev/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")),
   "component---src-pages-404-jsx": preferDefault(require("/Users/luca/Desktop/work/nibyou/code/k-b.dev/src/pages/404.jsx")),
   "component---src-pages-impressum-jsx": preferDefault(require("/Users/luca/Desktop/work/nibyou/code/k-b.dev/src/pages/impressum.jsx"))
